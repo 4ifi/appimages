445b5ec6c33b1ae6f2cb7c1d5fc68df47994b784
4ifi <mihailch815@gmail.com>
2020-06-08 01:52:37 +1000

    Update supertuxkart_build_appimage.sh

--- supertuxkart_build_appimage.sh
+++ supertuxkart_build_appimage.sh
@@ -31,7 +31,8 @@ Categories=Game;
 EOF
 
 # Get app icon
-cp AppDir/data/supertuxkart_128.png icon.png
+#cp AppDir/data/supertuxkart_128.png icon.png
+touch icon.png
 
 # create app entrypoint
 echo -e \#\!$(dirname $SHELL)/sh >> AppDir/AppRun
